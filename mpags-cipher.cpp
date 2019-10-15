fcada84b844d539f9560dfcf4f27696da11a36f4
Thomas Latham <T.Latham@warwick.ac.uk>
2019-10-15 10:12:09 +0100

    TL review of day 1 homework

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -2,52 +2,104 @@
 #include <string>
 #include <vector>
 
-std::string convertIntToString(char x);
+// For std::isalpha and std::isupper
+#include <cctype>
+
+std::string convertIntToString(const char x);
 
 int main(int argc, char* argv[])
 {
-
+  // Convert the command-line arguments into a more easily usable form
   const std::vector<std::string> cmdLineArgs{argv, argv+argc};
 
+  // Add a typedef that assigns another name for the given type for clarity
+  typedef std::vector<std::string>::size_type size_type;
+  const size_type nCmdLineArgs {cmdLineArgs.size()};
+
   std::cout << "Command Line Arguments: ";
-  for(int i{0}; i < argc; ++i){
+  for(size_type i{0}; i < nCmdLineArgs; ++i){
     std::cout << cmdLineArgs[i];
-    if(i != (argc - 1)){
+    if(i != (nCmdLineArgs - 1)){
       std::cout << ", ";
     } else {
       std::cout << "\n";
     }
   }
 
+  // Options that might be set by the command-line arguments
+  bool helpRequested {false};
+  bool versionRequested {false};
   std::string inputFileName{};
   std::string outputFileName{};
  
-  for(int i{0}; i < argc; ++i){
+  // Start loop from 1 rather than 0 to skip over the program name
+  for(size_type i{1}; i < nCmdLineArgs; ++i){
     if(cmdLineArgs[i] == "-h" || cmdLineArgs[i] == "--help"){
-      std::cout << "HELP - Type an input, when finished press Enter and then Ctrl+D" << std::endl;
+      helpRequested = true;
     }
     else if(cmdLineArgs[i] == "-i"){
+      // Next element is filename unless -i is the last argument
+      if (i == nCmdLineArgs-1) {
+        std::cerr << "[Error] -i option requires a filename argument" << std::endl;
+        // Exit main with non-zero return to indicate failure
+        return 1;
+      }
       inputFileName = cmdLineArgs[i+1];
-      std::cout << "Input file name: " << inputFileName << std::endl;
+      ++i;
     }
     else if(cmdLineArgs[i] == "-o"){
+      // Next element is filename unless -o is the last argument
+      if (i == nCmdLineArgs-1) {
+        std::cerr << "[Error] -o option requires a filename argument" << std::endl;
+        // Exit main with non-zero return to indicate failure
+        return 1;
+      }
       outputFileName = cmdLineArgs[i+1];
-      std::cout << "Output file name: " << outputFileName << std::endl;
-    }
-    else if(cmdLineArgs[i].at(0) == '.'){
-      continue;
+      ++i;
     }
     else if(cmdLineArgs[i] == "--version"){
-      std::cout << "Version 1" << std::endl;
+      versionRequested = true;
     }
     else{
-      if(i != 0 && (cmdLineArgs[i-1] == "-i" || cmdLineArgs[i-1] == "-o")) continue;
-      std::cout << "Command line input: '"; 
-      for(int j{0}; j < cmdLineArgs[i].size(); ++j){
-         std::cout << cmdLineArgs[i].at(j);
-	 if(j == (cmdLineArgs[i].size() - 1)) std::cout << "' is not understood" << std::endl;
-      }
+      std::cerr << "[Error] command line input: '" << cmdLineArgs[i] << "' is not understood" << std::endl;
+      // exit main with non-zero return to indicate failure
+      return 1;
     }
+  }
+
+  // Handle help, if requested
+  if (helpRequested) {
+    // Line splitting for readability
+    std::cout
+      << "Usage: mpags-cipher [-i <file>] [-o <file>]\n\n"
+      << "Encrypts/Decrypts input alphanumeric text using classical ciphers\n\n"
+      << "Available options:\n\n"
+      << "  -h|--help        Print this help message and exit\n\n"
+      << "  --version        Print version information\n\n"
+      << "  -i FILE          Read text to be processed from FILE\n"
+      << "                   Stdin will be used if not supplied\n\n"
+      << "  -o FILE          Write processed text to FILE\n"
+      << "                   Stdout will be used if not supplied\n\n"
+      << "If providing input on stdin, when finished press Enter and then Ctrl+D\n" << std::endl;
+    // Help requires no further action, so return from main
+    // with 0 used to indicate success
+    return 0;
+  }
+
+  // Handle version, if requested
+  // Like help, requires no further action,
+  // so return from main with zero to indicate success
+  if (versionRequested) {
+    std::cout << "Version 1" << std::endl;
+    return 0;
+  }
+
+  // Read in user input from stdin/file
+  // Warn that input file option not yet implemented
+  if (!inputFileName.empty()) {
+    std::cout << "[Warning] input from file ('"
+      << inputFileName
+      << "') not implemented yet, using stdin\n";
   }
 
   char in_char{'x'};
@@ -67,11 +119,22 @@ int main(int argc, char* argv[])
 
   }
 
+  // Output the transliterated text
+  // Warn that output file option not yet implemented
+  if (!outputFileName.empty()) {
+    std::cout << "[Warning] output to file ('"
+      << outputFileName
+      << "') not implemented yet, using stdout\n";
+  }
+
   std::cout << "Output String: " << outputString << std::endl;
 
+  // No strict requirement to return from main, but we recommend to do so for
+  // clarity and for consistency with other functions
+  return 0;
 }
 
-std::string convertIntToString(char x){
+std::string convertIntToString(const char x){
 
   std::string returnString{};
 
@@ -107,12 +170,10 @@ std::string convertIntToString(char x){
     returnString =  "NINE";
     break;
   default:
-    std::cout << "NOT A SINGLE DIGIT INT, RETURNING 0" << std::endl;
-    returnString =  "ERROR";
+    std::cerr << "[Error] NOT A SINGLE DIGIT INT, RETURNING EMPTY STRING" << std::endl;
     break;
   }
   
   return returnString;
-
 }
 
